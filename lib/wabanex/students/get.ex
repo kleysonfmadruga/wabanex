4718f8c6bdfbdda61e93dff085cb22ded07f95aa
kleysonfmadruga <kleysonfmadruga26@gmail.com>
2021-06-26 14:57:45 -0300

    Adds data preloading function to preload Training and Instructor structs in the Student struct

--- lib/wabanex/students/get.ex
+++ lib/wabanex/students/get.ex
@@ -1,8 +1,10 @@
 defmodule Wabanex.Students.Get do
   @moduledoc false
 
+  import Ecto.Query
+
   alias Ecto.UUID
-  alias Wabanex.{Repo, Student}
+  alias Wabanex.{Repo, Student, Training}
 
   def call(id) do
     id
@@ -13,10 +15,19 @@ defmodule Wabanex.Students.Get do
   defp handle_result({:ok, uuid}) do
     case Repo.get!(Student, uuid) do
       nil -> {:error, "Student not found"}
-      student -> {:ok, student}
+      student -> {:ok, preload_data(student)}
     end
   end
 
   defp handle_result(:error), do: {:error, "Invalid Uuid"}
 
+  defp preload_data(student) do
+    today = Date.utc_today()
+
+    trainings = from training in Training,
+            where: ^today >= training.start_date and ^today <= training.end_date
+
+    Repo.preload(student, :instructor)
+    |> Repo.preload([trainings: {first(trainings, :inserted_at), [:exercises]}])
+  end
 end
